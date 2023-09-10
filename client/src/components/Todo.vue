aaa3d0089f42b531f5249f3f3676fdba9376682e
yaasir cheekoory <yaasir1997@gmail.com>
2023-09-10 22:31:11 +0400

    more works done on index and show

--- client/src/components/Todo.vue
+++ client/src/components/Todo.vue
@@ -2,9 +2,25 @@
 import { ref, onMounted } from 'vue';
 const todos = ref([]);
 const API_URL = 'http://127.0.0.1:3000/todos';
+const title = ref();
+const body = ref();
+const status = ref(false);
+const todo_id = ref(0);
+const isEditing = ref(false);
+
 
 const updateTodo = async () => {
   return true;
+}
+
+const index = async () => {
+  const res = await fetch(API_URL, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  todos.value = await res.json();
 }
 
 const createTodo = async () => {
@@ -16,9 +32,21 @@ const createTodo = async () => {
     body: JSON.stringify({
       title: title.value,
       body: body.value,
+      status: status.value,
     })
   })
+
+  const data = await res.json();
+  todos.value.push(data);
+  title.value = '';
+  body.value = '';
+  todo_id.value = 0;
 }
+
+
+onMounted(() => {
+  index();
+})
 </script>
 
 <template>
@@ -28,7 +56,7 @@ const createTodo = async () => {
       <input type="text" v-model="title" class="title-input" placeholder="Title...">
       <input type="text" v-model="body" class="body-input" placeholder="Description...">
       <div class="action-btns">
-        <button>Create</button>
+        <button @click="createTodo" :disabled="title">Create</button>
         <button v-if="isEditing" @click="updateTodo">Update</button>
         <button v-if="isEditing">Cancel</button>
       </div>
@@ -36,11 +64,14 @@ const createTodo = async () => {
   
     <div class="list-container">
       <h2>My Todos:</h2>
-      <div class="list-item">
-        <p>Hello World, I have been coding since a while.</p>
-        <div class="item-btns">
-          <button>Edit</button>
-          <button>Delete</button>
+      <div class="list-items">
+        <div v-for="todo in todos" class="list-item">
+          <p>{{ todo.title }}</p>
+          <p>{{ todo.body }}</p>
+          <div class="item-btns">
+            <button>Edit</button>
+            <button>Delete</button>
+          </div>
         </div>
       </div>
     </div>
@@ -64,7 +95,7 @@ const createTodo = async () => {
 
 .title-input,
 .body-input {
-  width: 100%;
+  width: 80%;
   padding: 12px 20px;
   box-sizing: border-box;
   border: 1px solid purple;
@@ -84,6 +115,14 @@ const createTodo = async () => {
   flex-direction: column;
 }
 
+.list-items {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 1.2rem;
+}
+
 .list-item {
   border: 1px solid lightcyan;
   padding: 1.2rem 2rem;
@@ -94,6 +133,6 @@ const createTodo = async () => {
   display: flex;
   justify-content: flex-start;
   align-items: center;
-  gap: 1rem;
+  gap: .6rem;
 }
 </style>
--- db/migrate/20230910172153_remove_status_from_todos.rb
+++ db/migrate/20230910172153_remove_status_from_todos.rb
@@ -1,4 +1,5 @@
 class RemoveStatusFromTodos < ActiveRecord::Migration[7.0]
   def change
+    remove_column :todos, :status, :boolean
   end
 end
